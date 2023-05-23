1a4573259e2761c3e469c67c487401a1ec09047c
GMBlackjack <67909612+GMBlackjack@users.noreply.github.com>
2023-05-23 15:21:51 -0700

    FIRST COMMIT EVER WOO! (Setup)

---
+++ .vscode/new.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World\n";
+    cout<<"Hello World\n";
+    cout<<"Hello World WONK\n";
+    cout<<"Hello World\n";
+
+    //sweet it works.
+
+    return 0;
+}
