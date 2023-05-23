beaeca76ff4f2fbec0b160868032c0de999e6d01
Gabriel Steward <GMSeskii@k2so.barnesos.net>
2023-05-23 15:27:17 -0700

    Check from the Other Side

---
+++ New.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World\n";
+
+    int aVeryLongVariableName = 1;
+
+    aVeryLongVariableName = 2;
+
+    cout << aVeryLongVariableName << "\n";
+    
+    return 0;
+}
